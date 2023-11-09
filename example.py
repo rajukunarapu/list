11097d1eb611d6a1aef4c0a33788f83fc83ed063
rajukunarapu <149881592+rajukunarapu@users.noreply.github.com>
2023-11-09 23:08:17 +0530

    new pracice

---
+++ example.py
@@ -0,0 +1,25 @@
+# variables
+a = "white"
+print(a)
+red = blue = green = "color"
+print(blue)
+print(red)
+print(green)
+japan, italy, china = "hiroshima nagasakhi", "europe", "asia"
+print(japan)
+print(italy)
+print(china)
+
+
+# sting methods
+string = " This is a string "
+print(string[0])
+print(string[-1])
+print(string[2:])
+print(string.upper())
+print(string.lower())
+print(string.strip())
+print(string.capitalize())
+print(string.count('s'))
+print(string.replace("s","S"))
+print(string.title())
